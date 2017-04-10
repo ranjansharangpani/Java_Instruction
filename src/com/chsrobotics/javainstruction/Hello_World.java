5c8f6c6c8b98bacf3efcadd6576fcaf728965d3e
Mark Burrell (ORF) <markrb060@gmail.com>
2017-04-10 11:13:34 -0700

    Commit to set up Project in Eclipse, as well as creation of first class

---
+++ src/com/chsrobotics/javainstruction/Hello_World.java
@@ -0,0 +1,12 @@
+package com.chsrobotics.javainstruction;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
