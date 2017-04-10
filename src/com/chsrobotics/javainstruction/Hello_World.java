8a971b6d12e2011c6783ca458ce554f15a0578a9
Mark Burrell (ORF) <markrb060@gmail.com>
2017-04-10 12:26:45 -0700

    added a comment

--- src/com/chsrobotics/javainstruction/Hello_World.java
+++ src/com/chsrobotics/javainstruction/Hello_World.java
@@ -3,9 +3,8 @@ package com.chsrobotics.javainstruction;
 public class Hello_World {
 
 	public static void main(String[] args) {
-		
+		//prints a string to the console
 		System.out.println("Hello World");
-		// TODO Auto-generated method stub
 
 	}
 
