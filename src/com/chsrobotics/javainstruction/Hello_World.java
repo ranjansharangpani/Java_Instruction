c13280cd7ceffff36ae6d7c01988ce5e46afff53
ADSERV\sharangpanir <sharangpanir@students.osd.wednet.edu>
2017-04-14 13:04:20 -0700

    I copied the format of this program to create a personalized method and learned the errors of this particular programming.

--- src/com/chsrobotics/javainstruction/Hello_World.java
+++ src/com/chsrobotics/javainstruction/Hello_World.java
@@ -1,11 +1,11 @@
 package com.chsrobotics.javainstruction;
 
-public class Hello_World {
-
+public class Hello_World
+{
 	public static void main(String[] args) {
-		//prints a string to the console
-		System.out.println("Hello World");
 
+		System.out.println("Hello World my name is Powerbear");
 	}
-
 }
+
+
